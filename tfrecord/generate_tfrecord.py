40528df64316a4e5e2c3e7a8c1f8c3c0280017b7
trangml <mattluutrang@vt.edu>
2021-12-01 13:48:39 -0500

    Update tfrecord

--- tfrecord/generate_tfrecord.py
+++ tfrecord/generate_tfrecord.py
@@ -31,6 +31,10 @@ FLAGS = flags.FLAGS
 def class_text_to_int(row_label):
     if row_label == 'inverter':
         return 1
+    elif row_label == 'charge_controller':
+        return 2
+    elif row_label == 'battery':
+        return 3
     else:
         None
 
